eede0ce7798b03e824ba67c4e7ba9ce685ccabc5
sureface <jasurdjuraev2002@gmail.com>
2021-12-16 10:35:09 +0300

    album made dynamic

--- src/components/gallerySection.js
+++ src/components/gallerySection.js
@@ -1,12 +1,9 @@
 import React, {useState} from 'react';
 
 //styles
-import "../styles/gallerySection.scss"
+import "../styles/gallerySection.scss";
 
-//icons
-import {MdClose} from "react-icons/md"
-
-
+//overview images
 import viewImage1 from "../assets/images/image1.jpg"
 import viewImage2 from "../assets/images/image2.jpg"
 import viewImage3 from "../assets/images/image3.jpg"
@@ -98,9 +95,13 @@ import album9and5 from "../assets/images/albume9/tg9-5.jpg"
 import album9and6 from "../assets/images/albume9/tg9-6.jpg"
 import album9and7 from "../assets/images/albume9/tg9-7.jpg"
 
+// modal
+import Modal from "../pages/modal";
+
 
 const GallerySection = () => {
 
+    // Data
     const [data, setData] = useState({
         activeObject: null,
         object: [
@@ -143,49 +144,28 @@ const GallerySection = () => {
         ]
     })
 
-    //const [closeM, setCloseM] = useState(false);
+    // check click state
+    const [clicked, setClicked] = useState(false);
 
+    // closeModal function
     function closeModal() {
-        setData({ ...data, activeObject: null })
+        setData({...data, activeObject: null});
+        setClicked(false);
     }
 
-
-
+    //  clicked album
     function toggleActive(index) {
-        setData({ ...data, activeObject: data.object[index] })
-    }
-    
-    function toggleActiveStyles(index) {
-        if (data.object[index] === data.activeObject) {
-            return "modal px-5 active";
-        } else{
-            return "modal hidden";
-        }
+        setData({...data, activeObject: data.object[index]});
+        setClicked(true);
     }
 
     return (
-
          <section className="gallerySection">
+             { clicked ? <Modal data={data.activeObject} closeModal={closeModal}/> : ""}
             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 w-full">
                 {data.object.map((item, index) => {
-                    return(
+                    return (
                         <div key={index}>
-                            <div className={toggleActiveStyles(index)}>
-                                <MdClose className="text-white text-4xl sm:text-4xl md:text-4xl lg:text-4xl xl:text-5xl mx-3 my-3 cursor-pointer fixed top-0 lg:top-2 right-2" onClick={closeModal} />
-                                <div className="modal_body w-full flex flex-col items-center text-center">
-                                    <h2 className="text-white text-2xl sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl font-extrabold mb-5">{item.album[0].headerText}</h2>
-                                    <p className="text-white text-md sm:text-lg md:text-lg lg:text-xl xl:text-xl mb-5">
-                                        {item.album[0].pText}
-                                    </p>
-                                    <img className="mb-10" src={item.album[0].aUrl1} alt="oops sorry something wrong"/>
-                                    <img className="mb-10" src={item.album[0].aUrl2} alt="oops sorry something wrong"/>
-                                    <img className="mb-10" src={item.album[0].aUrl3} alt="oops sorry something wrong"/>
-                                    <img className="mb-10" src={item.album[0].aUrl4} alt="oops sorry something wrong"/>
-                                    <img className="mb-10" src={item.album[0].aUrl5} alt="oops sorry something wrong"/>
-                                    <img className="mb-10" src={item.album[0].aUrl6} alt="oops sorry something wrong"/>
-                                    <img className="mb-10" src={item.album[0].aUrl7} alt="oops sorry something wrong"/>
-                                </div>
-                            </div>
                             <div className="image-card w-full">
                                 <div className="image-card_overview cursor-pointer" onClick={() => toggleActive(index)}>
                                     <img className="w-full" src={item.imgUrl} alt="oops sorry something wrong"/>
@@ -193,7 +173,7 @@ const GallerySection = () => {
                                 </div>
                             </div>
                         </div>
-                    )
+                    );
                 })}
             </div>
         </section>
